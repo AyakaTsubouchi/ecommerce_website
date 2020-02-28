142486ad020d15d483154fd7fb353c990002261d
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-02-28 14:53:21 -0800

    add NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Shopper
+      </a>
+      <button type="submit" className="btn btn-outline-success my-2 my-sm-0">
+        Car
+      </button>
+    </nav>
+  );
+};
+export default NavBar;
